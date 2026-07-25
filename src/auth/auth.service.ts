f8b9671c5740dd199d93ead884b9b33770b25c12
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-07-25 17:29:36 -0300

    refactor: estandarizar la estructura de la response de autenticación

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,120 +1,116 @@
-import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Repository } from 'typeorm';
-import * as bcrypt  from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-
-
+import { promises } from 'dns';
+import { AuthResponseDto } from './dto/auth-response.dto';
 
 @Injectable()
 export class AuthService {
-  
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    private readonly jwtService: JwtService
-
-  ){}
+    private readonly jwtService: JwtService,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
-   
     try {
-      
-      const { password, ...userData } = createUserDto
-      
-      const user = this.userRepository.create( {
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
         ...userData,
-        password: bcrypt.hashSync( password, 10 )
-      } );
+        password: bcrypt.hashSync(password, 10),
+      });
 
-      const saveUser= await this.userRepository.save( user ) ;
+      const saveUser = await this.userRepository.save(user);
 
-      const { password:_ , ...restUser } = saveUser
+      const { password: _, ...restUser } = saveUser;
 
       return {
         ...restUser,
-        token: this.getJwt({ id: restUser.id })
-      }
-
+        token: this.getJwt({ id: restUser.id }),
+      };
     } catch (error) {
-      this.handleDbErrors( error );
+      this.handleDbErrors(error);
     }
-
   }
 
-  async login( loginUserDto: LoginUserDto ){
-
+  async login(loginUserDto: LoginUserDto): Promise<AuthResponseDto> {
     const { email, password } = loginUserDto;
 
     const user = await this.userRepository.findOne({
-      select: { email: true, password: true, fullName: true, isActive: true, roles: true, id: true, },
+      select: {
+        email: true,
+        password: true,
+        fullName: true,
+        isActive: true,
+        roles: true,
+        id: true,
+      },
       where: { email },
     });
 
-    if ( !user )
-      throw new UnauthorizedException('Credentials are not valid (email)')
+    if (!user)
+      throw new UnauthorizedException('Credentials are not valid (email)');
 
-    if ( !bcrypt.compareSync( password, user.password) )
-      throw new UnauthorizedException('Credentials are not valid (password)')
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid (password)');
 
-    const { id, ...userResponse } = user
-    
-    return {
-      ...userResponse,
-      token: this.getJwt({ id })
-    }
-    
+    return this.buildAuthResponse(user);
   }
 
-  async getUsers(paginationDto: PaginationDto){
+  private buildAuthResponse(user: User): AuthResponseDto {
+    const { id, password: _, ...userResponse } = user;
 
-    const { limit= 10, offset= 0 } = paginationDto
+    return {
+      user: { id, ...userResponse },
+      token: this.getJwt({ id }),
+    };
+  }
+
+  async getUsers(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
 
     const users = await this.userRepository.find({
-      
       take: limit,
       skip: offset,
+    });
 
-    })
-
-    return users.map( ( user ) => ({
+    return users.map((user) => ({
       ...user,
-    }))    
-
+    }));
   }
 
-  async checkAuthStatus( user: User ){
-    
+  async checkAuthStatus(user: User) {
     return {
       ...user,
-      token: this.getJwt({ id: user.id })
-    }
-    
+      token: this.getJwt({ id: user.id }),
+    };
   }
 
-
-  private getJwt( payload: JwtPayload ){
-
-    const token = this.jwtService.sign( payload );
+  private getJwt(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
     return token;
-
   }
 
-  private handleDbErrors( error: any ): never {
-    if ( error.code === '23505' )
-      throw new BadRequestException( error.detail );
+  private handleDbErrors(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
 
     // console.log(error);
 
-    throw new InternalServerErrorException('Please check server logs')
-
+    throw new InternalServerErrorException('Please check server logs');
   }
-
 }
---
+++ src/auth/dto/user-response.dto.ts
@@ -0,0 +1,7 @@
+export interface UserResponseDto {
+  id: string;
+  email: string;
+  fullName: string;
+  isActive: boolean;
+  roles: string[];
+}
