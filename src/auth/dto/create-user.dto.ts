aa0608a765e23d6c7402353a8b47f56aad50fa6b
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-07-25 17:32:50 -0300

    refactor: mejoras en la entrada de validacion de autenticación

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,27 +1,33 @@
-import { IsArray, IsEmail, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import {
+  IsArray,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-
   @IsString()
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsString()
   @MinLength(6)
   @MaxLength(50)
-  @Matches(
-      /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-      message: 'The password must have a Uppercase, lowercase letter and a number'
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
   })
-  password: string;
+  password!: string;
 
   @IsString()
   @MinLength(1)
-  fullName: string;
+  fullName!: string;
 
   @IsString({ each: true }) // 1. Valida que CADA item del array sea un string
-  @IsArray()                // 2. Valida que la propiedad en sí misma sea un array
-  @IsOptional()             // 3. Opcional, porque en tu Entity ya tienes un default
+  @IsArray() // 2. Valida que la propiedad en sí misma sea un array
+  @IsOptional() // 3. Opcional, porque en tu Entity ya tienes un default
   roles?: string[];
-
 }
